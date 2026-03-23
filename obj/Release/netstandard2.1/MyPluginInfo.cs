1342011f5dab04b1dc76ec11f8b2171c80896a8d
Endar728 <72927174+Endar728@users.noreply.github.com>
2026-03-23 16:49:47 -0500

    Delete obj directory

--- obj/Release/netstandard2.1/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace EditorPlus
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "com.nikkorap.EditorPlus";
-        public const string PLUGIN_NAME = "editorplus";
-        public const string PLUGIN_VERSION = "1.4.2";
-    }
-}
